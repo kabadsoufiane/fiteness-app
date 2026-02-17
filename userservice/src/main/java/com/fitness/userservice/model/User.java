13dba502d54eefb337c66662986eca641fd363c7
kabadsoufiane <kabadsoufiane@gmail.com>
2026-02-17 15:12:18 +0100

    Accommodate Keycloak ID into UserService

--- userservice/src/main/java/com/fitness/userservice/model/User.java
+++ userservice/src/main/java/com/fitness/userservice/model/User.java
@@ -17,6 +17,8 @@ public class User {
     @Column(unique = true, nullable = false)
     private String email;
 
+    private String keycloakId;
+
     @Column(nullable = false)
     private String password;
 
